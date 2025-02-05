39993c9ca593774d29fc699a826e5a578f7430bd
Paulinus Mfon <59096300+mfontosco@users.noreply.github.com>
2025-02-05 16:09:47 +0100

    added api to the route

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ const getDigitSum = (num) => {
     return num.toString().split("").reduce((sum, digit) => sum + parseInt(digit), 0);
 };
 
-app.get("/number", async (req, res) => {
+app.get("/api/classify-number", async (req, res) => {
     const { number } = req.query;
     const num = parseInt(number);
     
