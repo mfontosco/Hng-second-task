7481743d64ea15b9708dc313b64e34e24987ae7d
Paulinus Mfon <59096300+mfontosco@users.noreply.github.com>
2025-02-05 16:32:41 +0100

    error fixes

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.get("/api/classify-number", async (req, res) => {
     const num = parseInt(number);
     
     if (isNaN(num)) {
-        return res.status(400).json({ number: num, error: true });
+        return res.status(400).json({ number: number, error: true });
     }
 
     const properties = [];
